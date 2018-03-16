c1a337c4aba9daa44aeb15962c6eb8eb18c3bd71
Matthew Gidden <matthew.gidden@gmail.com>
2018-03-16 16:11:56 +0100

    some files added

---
+++ make_pres.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+import subprocess
+import shutil
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("fname", help="Path to Notebook Presentation File")
+args = parser.parse_args()
+
+fpth = os.path.abspath(args.fname)
+if not os.path.exists(fpth):
+    raise IOError('{} does not exist'.format(fpth))
+
+# make notebook html
+cmd = "jupyter nbconvert {} --to slides --reveal-prefix={}"
+cmd = cmd.format(fpth, os.path.join(here, 'reveal.js'))
+fdir = os.path.dirname(fpth)
+subprocess.check_call(cmd.split(), cwd=fdir)
+
+# move it to index.html
+bare = os.path.basename(fpth).split('.')[0]
+oldhtml = os.path.join(fdir, '{}.slides.html'.format(bare))
+newhtml = os.path.join(fdir, 'index.html')
+shutil.move(oldhtml, newhtml)
